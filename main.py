f0952e5132f686ae2e7e7ec3fadafba7a4a6e926
Aiden Favish <aidenfavish@gmail.com>
2023-03-18 14:19:05 -0700

    minor changes

--- main.py
+++ main.py
@@ -35,7 +35,7 @@ async def on_message(message):
     # Makes sure Jason doesn't read his own messages
     if message.author.id != client.user.id:
         if message.channel.id == channels.JASONGPT:
-            await message.channel.send(await JasonGPT.prompt(""))
+            await message.channel.send(await JasonGPT.prompt(message.content))
         else:
             await commands.custom_commands(message, client)
 
